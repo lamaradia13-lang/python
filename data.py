b16d9285a803b7ae769035579912036e292f7205
kapi <lamaradia13@gmail.com>
2026-01-24 00:29:06 -0500

    dictionnary file

---
+++ data.py
@@ -0,0 +1,5 @@
+# Creating a list of cars:
+cars = ["ford", "bmw", "ferrari", "mercedez", "lamborghini"] 
+cars.append("bugatti")
+cars.pop(0)
+print(cars)
---
+++ dict.py
@@ -0,0 +1,6 @@
+person = {"name": "kapi", "age": "90", "city": "nyc", "job": "software enginner"}
+person.pop("job")
+print(person["name"])
+print(person["age"])
+print(person["city"])
+print(person)
